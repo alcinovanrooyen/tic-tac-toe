e79643570f8efb988e72b53564f71a537d3514df
Ubuntu <ubuntu@ip-172-31-21-255.eu-west-1.compute.internal>
2023-05-30 14:34:49 +0000

    modularized javscript

--- index.js
+++ index.js
@@ -1,72 +1,79 @@
-let nextShape = 'X', messageSection;
-const winCombs = [
-    [1,2,3], [4,5,6], [7,8,9],  //  Horizontal
-    [1,4,7], [2,5,8], [3,6,9],  //  Vertical
-    [1,5,9], [3,5,7]            //  Diagnoal
-];
-
-
-function startGame () {
-    const blocks = document.querySelectorAll('#board > div');
-    blocks.forEach(block => block.addEventListener('click', blockClicked));
-    
-    const resets = document.querySelectorAll('.reset-btn');
-    resets.forEach(reset => reset.addEventListener('click', () => resetGame(blocks)));
-    
-    messageSection = document.querySelector('#message');
-}
-
-function resetGame(blocks) {
-    clearBlocks(blocks);
-    nextShape = 'X';
-    messageSection.style.display = 'none';
-    
-}
-
-function clearBlocks(blocks) {
-    blocks.forEach(block=>block.innerText = "");
-}
-
-function blockClicked (e) {
-    e.target.innerText = nextShape;
-    nextShape = nextShape == 'X' ? 'O' : 'X';
-    checkDidWin();
-}
-
-function checkDidWin () {
-    const xWon = check('X');
-    if (xWon) 
-        return endGame('X');
-
-    const yWon = check('O');
-    if (yWon) 
-        return endGame('O');
+class TicTacToe {
+    constructor() {
+        this.nextShape = 'X';
+        this.messageSection = document.querySelector('#message');
+        this.blocks = document.querySelectorAll('#board > div');
+        this.resets = document.querySelectorAll('.reset-btn');
         
-    // Todo: Add a counter, check if it is 9, then declare tie.
-}
+        this.winCombs = [
+            [1,2,3], [4,5,6], [7,8,9],  //  Horizontal
+            [1,4,7], [2,5,8], [3,6,9],  //  Vertical
+            [1,5,9], [3,5,7]            //  Diagnoal
+        ];
+    }
 
-function check(shape) {
-    let cBlock, matches;
-    for (let i = 0; i < winCombs.length; i++) {
-        // For each direction reset matches to zero
-        matches = 0;
-        for (let n = 0; n < winCombs[i].length; n++) {
-            cBlock = document.querySelector(`#board > div:nth-child(${winCombs[i][n]})`);
-            // Tally when shape occurs 3 times in a row
-            if (cBlock.innerText == shape) 
-                matches++;
-        }
-        // If matches is equal to 3 for a certain direction, then they win.
-        if (matches == 3)
-            return true;
+
+    startGame () {
+        this.blocks.forEach(block => block.addEventListener('click', e => this.blockClicked(e) ));
+        this.resets.forEach(reset => reset.addEventListener('click', () => this.resetGame() ));
+    }
+
+    
+    clearBlocks() {
+        this.blocks.forEach(block=>block.innerText = "");
+    }
+        
+    resetGame() {
+        this.clearBlocks(this.blocks);
+        this.nextShape = 'X';
+        this.messageSection.style.display = 'none';
+        
     }
     
-    return false;
+    blockClicked (e) {
+        e.target.innerText = this.nextShape;
+        this.nextShape = this.nextShape == 'X' ? 'O' : 'X';
+        this.checkDidWin();
+    }
+    
+    checkDidWin () {
+        const xWon = this.check('X');
+        if (xWon) 
+            return this.endGame('X');
+    
+        const yWon = this.check('O');
+        if (yWon) 
+            return this.endGame('O');
+            
+        // Todo: Add a counter, check if it is 9, then declare tie.
+    }
+    
+    check(shape) {
+        let cBlock, matches;
+        for (let i = 0; i < this.winCombs.length; i++) {
+            // For each direction reset matches to zero
+            matches = 0;
+            for (let n = 0; n < this.winCombs[i].length; n++) {
+                cBlock = document.querySelector(`#board > div:nth-child(${this.winCombs[i][n]})`);
+                // Tally when shape occurs 3 times in a row
+                if (cBlock.innerText == shape) 
+                    matches++;
+            }
+            // If matches is equal to 3 for a certain direction, then they win.
+            if (matches == 3)
+                return true;
+        }
+        
+        return false;
+    }
+    
+    endGame(shape) {
+        document.querySelector('#message > div > div:first-child')
+            .innerText = `Player ${shape == 'X' ? '1' : '2'} won!`;
+          
+        this.messageSection.style.display = 'flex';
+    }
+    
 }
 
-function endGame(shape) {
-    document.querySelector('#message > div > div:first-child')
-        .innerText = `Player ${shape == 'X' ? '1' : '2'} won!`;
-      
-    messageSection.style.display = 'flex';
-}
+export default TicTacToe;
